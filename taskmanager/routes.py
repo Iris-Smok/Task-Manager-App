85a5abbebe3727e229a0fc1e65db96959d3100ab
Iris-Smok <irissmok11@gmail.com>
2022-03-08 15:45:00 +0000

    Edit categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -39,3 +39,16 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    """
+    edit_category.html
+    """
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
