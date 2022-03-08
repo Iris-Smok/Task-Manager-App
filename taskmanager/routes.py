455bb707f194bbc126eeaa1898c6909a218a5672
Iris-Smok <irissmok11@gmail.com>
2022-03-08 14:15:25 +0000

    Add categories.html and add_category.html, add routes for both pages

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -13,3 +13,23 @@ def home():
     brought to the tasks page
     """
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    """
+    Categories.html
+    """
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    """
+    When user click to add a new category it should render
+    the template that contains a form. And by displaying the form to users
+    this uses the GET method to get the page. Whrn user submits this form,
+    the form will attempt to post the data into the database.
+    This is why we need to specify both methods in the app route
+    """
+    return render_template("add_category.html")
