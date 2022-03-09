ea3fb4898b269925952ffa300051561f1a5e8d28
Iris-Smok <irissmok11@gmail.com>
2022-03-09 16:16:28 +0000

    Add task to database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
   // sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  var datapicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datapicker, {
+    format: "dd mm, yyyy",
+    i18n: { dpne: "Select" },
+  });
+
+  // select initialization
+  var selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
 });
 
+// modal initialization
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".modal");
   var instances = M.Modal.init(elems, (opacity = 0.5));
 });
+
+//
